ed18e218ecefcaa329a94f98ce015783252c8520
Aiden <amb@aspy.dev>
2024-10-23 23:02:42 -0400

    Seems alright

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 git submodule update --init --recursive;
-cd src/frida-ios-dump || echo "Failed to enter frida-ios-dump!" && exit 1;
-python -m venv ./.venv;
+mkdir IPAs
+cd src/frida-ios-dump || exit 1
+python -m venv ./.venv
 # shellcheck disable=SC1091
-source ./venv/bin/activate;
-pip install frida frida-tools;
+source ./.venv/bin/activate
+pip install frida frida-tools
 chmod +x src/GetIPA.sh
-echo done!;
-
-exit 0;
+echo done!
+exit 0
--- src/GetIPA.sh
+++ src/GetIPA.sh
@@ -4,7 +4,8 @@ function getEnv() {
     grep "$1" ./../.env | cut -d "=" -f2;
 }
 
-SSH=$(getEnv FridaUser)@$(getEnv FridaIP);
+ideviceIP=$(getEnv ideviceIP);
+SSH="$(getEnv ideviceUser)@${ideviceIP} -p $(getEnv idevicePort)";
 IPAServer=$(getEnv IPAServer);
 
 build=$(ssh -q "$SSH" -t "plutil -key CFBundleVersion /var/containers/Bundle/Application/654EF313-6AC0-45D2-BB2A-BC328A920F26/Discord.app/Info.plist" | tr -d '\n' | tr -d '\r');
@@ -14,7 +15,15 @@ IPAHostStatus=$(curl -Is "${IPAServer}"/Discord_"${version}"_"${build}".ipa | aw
 
 if [ "$IPAHostStatus" == "200" ]
     then
-        echo "Discord_${version}_${build}.ipa exists already!"
+        echo "Discord_${version}_${build}.ipa exists already!";
         exit 0;
 fi
 
+ssh -q "$SSH" -t "open ${1}"
+
+# shellcheck disable=SC1091
+source src/frida-ios-dump/.venv/bin/activate
+
+python src/frida-ios-dump/decrypter.py -H "${FridaIP}" -N "${1}" -O IPAs/Discord_"${version}"_"${build}".ipa 
+
+rsync -aPe "ssh -p $(getEnv UploadServerPort)" --stats IPAs/Discord_"${version}"_"${build}".ipa "$(getEnv UploadServerUser)"@"$(getEnv UploadServerIP)":"$(getEnv UploadDirectory)"
